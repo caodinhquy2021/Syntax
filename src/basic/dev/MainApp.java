434fd591676a75ee07160db44cd54ca4319229bd
quy <caodinhquyaaa1@gmail.com>
2021-10-25 14:39:44 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,35 @@
+package basic.dev;
+/**
+* Ghi chú (comment nhiều dòng)
+* @author.CaoDinhQuy
+* 1. Tên lớp trùng tên file
+* 2. Tên lớp chứ cái đầu viết hoa. Nếu từ ghép thì những chữ cái đầu biết hoa
+* 3. Tên lớp đặt phải có ý nghĩa. Ví dụ: class Peson : lớp mô tả con người
+* - Interface: Phải có tiền tố I. Ví dụ : IListener
+* - Abstract : Phải có tiền tố Abstract. Ví dụ: AbstractShape
+*/
+public class MainApp {
+	//Comment 1 dòng cách 1. Phím nóng Ctr + /
+			/*Comment 1 dòng cách 2 2*/
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Hello World");
+	/**
+	 * Cách đặt tên biến:
+	 * 1. Không chứa ký tự đatcự biệt !@#$%^&*()_+=
+	 * 2. Không chứa khoẳng trắng
+	 * 3. Viết thường
+	 * 4. Không được bắt đầu bằng số
+	 */
+	int number;
+	String nameSchool;
+	}
+	/*
+	 *  tên phương thức tuân theo quy tắc camelCase
+	 */
+	public void show() {
+		
+	}
+
+}
